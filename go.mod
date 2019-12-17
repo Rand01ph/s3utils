6fe2bb2c506d4b4f56694a890f85f61ad108b6e1
Rand01ph <tanyawei1991@gmail.com>
2019-12-17 16:38:45 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module S3Archive
+
+go 1.13
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/minio/minio-go/v6 v6.0.44
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
---
+++ s3archive.go
@@ -0,0 +1,79 @@
+package S3Archive
+
+import (
+	"archive/zip"
+	"fmt"
+	"github.com/minio/minio-go/v6"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func S3PrefixZip(s3Client *minio.Client, bucketName, objectPrefix, outZipName string) {
+
+	// 创建ZIP包
+	newZipFile, err := os.Create(outZipName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer newZipFile.Close()
+	zipWriter := zip.NewWriter(newZipFile)
+	defer zipWriter.Close()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("%#v\n", s3Client) // s3Client is now setup
+
+	buckets, err := s3Client.ListBuckets()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for _, bucket := range buckets {
+		log.Println(bucket)
+	}
+
+	doneCh := make(chan struct{})
+
+	defer close(doneCh)
+
+	isRecursive := true
+	objectCh := s3Client.ListObjectsV2(bucketName, objectPrefix, isRecursive, doneCh)
+	for object := range objectCh {
+		if object.Err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(object)
+		mObj, err := s3Client.GetObject(bucketName, object.Key, minio.GetObjectOptions{})
+		if err != nil {
+			log.Fatalln(err)
+		}
+		rel, err := filepath.Rel(objectPrefix, object.Key)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if err = AddFileToZip(zipWriter, rel, mObj); err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
+
+func AddFileToZip(zipWriter *zip.Writer, filename string, fileObject io.Reader) error {
+
+	header := &zip.FileHeader{
+		Name:         filename,
+		Method:       zip.Deflate,
+		ModifiedTime: uint16(time.Now().UnixNano()),
+		ModifiedDate: uint16(time.Now().UnixNano()),
+	}
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(writer, fileObject)
+	return err
+}
